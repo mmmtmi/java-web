bee01a2d5f1667da8cde7e7b1218669c047d1306
mmmtmi <minamii.tks@gmail.com>
2024-12-02 17:39:18 +0900

    test1201

---
+++ Test1201/src/main/java/Main.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher fi = request.getRequestDispatcher("WEB-INF/requ1.jsp");
+		fi.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		String name = request.getParameter("name");
+		request.setAttribute("n", name);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/for1.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
